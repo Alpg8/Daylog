a41f537261d0ebf704fa90a7e95431ed39a2f4cb
Alp <alp@unknown2e5521179a9a.home>
2026-04-17 03:50:21 +0300

    Next.js: allow R2 public domain for next/image

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-60c1b097ea13484f9c04938288582747.r2.dev",
+      },
+    ],
+  },
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
